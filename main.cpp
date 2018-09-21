ee079e480207033b73a439da67306783f25b89b2
alice221196 <alisa_karpova_2018@mail.ru>
2018-09-21 16:04:36 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+
+int main() {
+
+    char c='j';
+    int a = 50;
+
+    std::cout<<c/float(a)<<'\n';
+    std::cout<<c*a<<'\n';
+    std::cout<<c<<'\n';
+
+    return 0;
+}
